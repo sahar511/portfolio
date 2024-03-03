e3b41c2c32abeaf497df87f3295b12eff4031ea9
sahar511 <sahararef@sahars-Air.gateway.sasktel.net>
2024-03-03 10:59:35 -0600

    fix: fix contacts page

--- src/views/ContactMe/index.tsx
+++ src/views/ContactMe/index.tsx
@@ -1,17 +1,20 @@
+import styles from "./ContactMe.style.module.css"
+
 export default function ContactMe() {
   return (
     <div>
-      <form action="https://api.web3forms.com/submit" method="POST">
-        <input type="hidden" name="access_key" value="b5f7f479-b8c9-472a-b052-28af33ace587" />
-        <input type="text" name="name" required />
-        <input type="email" name="email" required />
-        <textarea name="message" required></textarea>
+      <form action="https://api.web3forms.com/submit" method="POST" className={styles.form}>
+        <h1 className={styles.title}>Contact Me</h1>
+        <input type="hidden" name="access_key"/>
+        <input type="text" name="name" required className={styles.input}  placeholder="Name"/>
+        <input type="email" name="email" required  className={styles.input}  placeholder="Email"/>
+        <textarea name="message" required className={`${styles.input} ${styles.textarea}`}  placeholder="Message"></textarea>
         <input
           type="hidden"
           name="redirect"
           value="https://web3forms.com/success"
         />
-        <button type="submit">Submit Form</button>
+        <button type="submit" className={styles.button}>Send</button>
       </form>
     </div>
   )
